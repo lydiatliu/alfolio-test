0121f141b67d5465c6c1b9e75cdfd92516ec4023
lydiatliu <tingruolydia@gmail.com>
2026-06-13 17:22:23 +0000

    Deploying to gh-pages from @ lydiatliu/lydiatliu.github.io@038d9751439b5a26637844329280d68b3f2a1a70 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -9,7 +9,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "lydiatliu/alfolio-test",
+    "data-repo": "lydiatliu/lydiatliu.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
